706f6eaae262187dffc9dbc97e614ee052bd1270
Aryan Pachore <aryanpachore2004@gmail.com>
2025-03-31 22:43:17 +0530

    character hashing

--- Hashmaps/hashing.cpp
+++ Hashmaps/hashing.cpp
@@ -1,8 +1,31 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-    
 
-   return 0;
+const int CHAR_SIZE = 256; // For all ASCII characters
+
+void characterHashing(const string& str) {
+    int hash[CHAR_SIZE] = {0};
+
+    // Calculate frequency of each character
+    for (char ch : str) {
+        hash[ch]++;
+    }
+
+    // Display the hash values
+    cout << "Character Frequency:" << endl;
+    for (int i = 0; i < CHAR_SIZE; i++) {
+        if (hash[i] > 0) {
+            cout << (char)i << ": " << hash[i] << endl;
+        }
+    }
+}
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    cin >> str; // Using cin instead of getline
+
+    characterHashing(str);
+
+    return 0;
 }
